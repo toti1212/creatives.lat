5ab6895178c803bf720bed2cdf5c4f8f6ef2d948
Rodrigo Suárez <rodrigosrz12@gmail.com>
2020-04-15 19:21:19 -0300

    Updates

---
+++ precache-manifest.6702fdf3f1f8ae079a8d03b90234e37b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a0385071983760435ac61bb70ed9b6",
+    "url": "/creatives.lat/index.html"
+  },
+  {
+    "revision": "a35319bf2e02ab3a1af1",
+    "url": "/creatives.lat/static/css/main.a8ae93dc.chunk.css"
+  },
+  {
+    "revision": "d036939b6df4a2eed78d",
+    "url": "/creatives.lat/static/js/2.d750c406.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/creatives.lat/static/js/2.d750c406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35319bf2e02ab3a1af1",
+    "url": "/creatives.lat/static/js/main.05e7260b.chunk.js"
+  },
+  {
+    "revision": "cf835e51baabee80c290",
+    "url": "/creatives.lat/static/js/runtime-main.f8cf0ad6.js"
+  }
+]);
