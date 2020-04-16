491c6362247556f4183bbe77ad53cd8952e5d7b3
Rodrigo Suárez <rodrigosrz12@gmail.com>
2020-04-16 17:03:23 -0300

    🛠  Update banner text

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -8,10 +8,17 @@ const Banner = () => (
       Creatives.lat <span role="img" aria-label="logo"></span>🎨
     </h1>
     <h3>
-      Dada la difícil situación que estamos atravesando, creemos que es un
-      buen momento para crear comunidad, así entre todos podemos ayudarnos{" "}
+      Dada la difícil situación que estamos atravesando, creemos que es un buen
+      momento para crear una comunidad, tender redes, trabajar en conjunto y
+      ayudarnos{" "}
       <span role="img" aria-label="logo">
         💪
+      </span>
+      <br />
+      Desarrollamos esta página con el fin de visibilizar y reunir en un solo
+      lugar a todas aquellas personas con perfil creativo{" "}
+      <span role="img" aria-label="emoji creativo">
+        📷🎨
       </span>
       <br />
       Si sos una persona creativa, dedicada al área audiovisual, publicidad y/o
