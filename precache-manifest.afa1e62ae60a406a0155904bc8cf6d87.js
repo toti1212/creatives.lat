73240a961e7559f873e4d2806b4216583795c478
Rodrigo Suárez <rodrigosrz12@gmail.com>
2020-04-16 17:51:19 -0300

    Updates

---
+++ precache-manifest.afa1e62ae60a406a0155904bc8cf6d87.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7e327d74a666a9042e6ac1dc77453a",
+    "url": "/creatives.lat/index.html"
+  },
+  {
+    "revision": "d8ae86b2b82c8c9cbe51",
+    "url": "/creatives.lat/static/css/main.143a0f25.chunk.css"
+  },
+  {
+    "revision": "b8a0bce487c54a692069",
+    "url": "/creatives.lat/static/js/2.e7733d11.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/creatives.lat/static/js/2.e7733d11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8ae86b2b82c8c9cbe51",
+    "url": "/creatives.lat/static/js/main.b79db179.chunk.js"
+  },
+  {
+    "revision": "cf835e51baabee80c290",
+    "url": "/creatives.lat/static/js/runtime-main.f8cf0ad6.js"
+  },
+  {
+    "revision": "98a81038f5cfc7a0b20352c68ff5b160",
+    "url": "/creatives.lat/static/media/pattern.98a81038.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d30a47cd8cd248624b581032876bad8.js"
+  "/creatives.lat/precache-manifest.afa1e62ae60a406a0155904bc8cf6d87.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/creatives.lat/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
