d94f27a55752c60391a619a1c51095fbdb8d8f27
Rodrigo Suárez <rodrigosrz12@gmail.com>
2020-04-20 23:58:34 -0300

    ⭐️ Rebranding
    - Added new logo and colors
    - Change fonts and texts
    - Adapt page based in new designs

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Creatives.lat 🎨
+<div id="theia-logo" align="center">
+    <br />
+    <img src="/images/logo.svg" alt="Creatives.lat Logo" width="300"/>
+    <h3>Creatives.lat 🎨</h3>
+</div>
 
 This website is an initiative to expand and create a network with creative professionals in Latin America 💪, and perhaps, we create new opportunities among us 🙂
 
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,21 +3,13 @@ import FolksList from "./FolksList/index.jsx";
 import Footer from "./Footer/index.jsx";
 import Header from "./Header/index.jsx";
 import React from "react";
-import bg from "../assets/pattern.svg"
-import styled from 'styled-components'
 
-const Wrapper = styled.div`
-background-image: url(${bg});
-background-attachment: scroll;
-`
 const App = () => (
   <React.Fragment>
     <Header />
-    <Wrapper>
-      <Banner />
-      <FolksList />
-      <Footer />
-    </Wrapper>
+    <Banner />
+    <FolksList />
+    <Footer />
   </React.Fragment>
 );
 
